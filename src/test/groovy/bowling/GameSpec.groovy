2c5edc434cea31b1bed9b4181d8871b0227f53e2
unknown <pbauer@.corp.shutterfly.com>
2011-06-29 22:50:47 -0700

    Re-simplified bowling DSL

--- src/test/groovy/bowling/GameSpec.groovy
+++ src/test/groovy/bowling/GameSpec.groovy
@@ -8,12 +8,12 @@ class GameSpec extends Specification {
     score == rolls.playGame(description).score
 
     where:
-    description    | rolls                             | score
-    "gutter game"  | roll.many(20,0)                   | 0
-    "all ones"     | roll.many(20,1)                   | 20
-    "one spare"    | roll.spare.three.many(17,0)       | 16
-    "one strike"   | roll.strike.three.four.many(16,0) | 24
-    "perfect game" | roll.many(12,10)                  | 300
+    description    | rolls                              | score
+    "gutter game"  | roll.many(20,0)                    | 0
+    "all ones"     | roll.many(20,1)                    | 20
+    "one spare"    | roll.spare().once(3).many(17,0)    | 16
+    "one strike"   | roll.strike().once(3,4).many(16,0) | 24
+    "perfect game" | roll.many(12,10)                   | 300
   }
 
   RollCollector getRoll() {
@@ -23,7 +23,6 @@ class GameSpec extends Specification {
 }
 
 class RollCollector {
-  static final numbers = [zero:0, one:1, two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9, ten:10]
   private def _rolls = []
   private Game game = new Game()
 
@@ -32,19 +31,13 @@ class RollCollector {
     return this
   }
 
-  RollCollector getStrike() { once(10) }
-  RollCollector getSpare() { once(5, 5) }
-
-  RollCollector many(int n, int pins) {
-    once((0..<n).collect {pins} as int[])
-  }
-
-  def propertyMissing(String name) {
-    numbers[name] ? once(numbers[name]) : this
-  }
+  RollCollector strike() { once(10) }
+  RollCollector spare() { once(5, 5) }
+  RollCollector many(int n, int pins) { once((0..<n).collect {pins} as int[]) }
 
   Game playGame(String description) {
     game.roll(_rolls as int[])
     return game
   }
 }
+
