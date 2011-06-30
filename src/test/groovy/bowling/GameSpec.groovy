ea95c0b0a121e4026423ece74b570ba7467e4448
unknown <pbauer@.corp.shutterfly.com>
2011-06-29 19:48:30 -0700

    Extended bowling DSL

--- src/test/groovy/bowling/GameSpec.groovy
+++ src/test/groovy/bowling/GameSpec.groovy
@@ -8,38 +8,43 @@ class GameSpec extends Specification {
     score == rolls.playGame(description).score
 
     where:
-    description    | rolls                              | score
-    "gutter game"  | roll.many(20,0)                    | 0
-    "all ones"     | roll.many(20,1)                    | 20
-    "one spare"    | roll.spare().once(3).many(17,0)    | 16
-    "one strike"   | roll.strike().once(3,4).many(16,0) | 24
-    "perfect game" | roll.many(12,10)                   | 300
+    description    | rolls                             | score
+    "gutter game"  | roll.many(20,0)                   | 0
+    "all ones"     | roll.many(20,1)                   | 20
+    "one spare"    | roll.spare.three.many(17,0)       | 16
+    "one strike"   | roll.strike.three.four.many(16,0) | 24
+    "perfect game" | roll.many(12,10)                  | 300
   }
 
   RollCollector getRoll() {
     new RollCollector()
   }
 
-  class RollCollector {
-    private def _rolls = []
-    private Game game = new Game()
-
-    RollCollector once(int... rolls) {
-      _rolls.addAll(rolls)
-      return this
-    }
-
-    RollCollector strike() { once(10) }
-    RollCollector spare() { once(5, 5) }
-
-    RollCollector many(int n, int pins) {
-      once((0..<n).collect {pins} as int[])
-    }
-
-    Game playGame(String description) {
-      game.roll(_rolls as int[])
-      return game
-    }
-  }
 }
 
+class RollCollector {
+  private def _rolls = []
+  private Game game = new Game()
+
+  RollCollector once(int... rolls) {
+    _rolls.addAll(rolls)
+    return this
+  }
+
+  RollCollector getStrike() { once(10) }
+  RollCollector getSpare() { once(5, 5) }
+
+  RollCollector many(int n, int pins) {
+    once((0..<n).collect {pins} as int[])
+  }
+
+  def propertyMissing(String name) {
+    static final numbers = [zero:0, one:1, two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9, ten:10]
+    numbers[name] ? once(numbers[name]) : this
+  }
+
+  Game playGame(String description) {
+    game.roll(_rolls as int[])
+    return game
+  }
+}
